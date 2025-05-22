2c48f32eb556f16aad4a05908cb91170b7181a01
Esteban <esteban.morauc@gmail.com>
2025-05-21 20:46:28 -0400

    Creación del microservicio Participacion

---
+++ src/main/java/com/grupo6/servicioParticipacion/ServicioParticipacionApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioParticipacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioParticipacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioParticipacionApplication.class, args);
+	}
+
+}
